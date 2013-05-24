40b67ef00c22773f8332310e5e034b09ef72df6c
Gonçalo Margalho <gsky89@gmail.com>
2013-05-24 10:59:02 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,3 @@
-/* 
- * File:        main.cpp
- * Author:      Gary Stafford
- * Description: Program entry point
- * Created:     February 3, 2013
- */
-
 #include <stdio.h>
 #include <sstream>
 #include <stdlib.h>
@@ -19,24 +12,6 @@ int main(int argc, char* argv[]) {
     int captureHeight = 800;
 
     DetectBlobsShowVideo(captureWidth, captureHeight);
-        // // test 3 and 4 don't require width and height parameters
-        // if (captureMethod != 3 && captureMethod != 4) {
-        //     cout << endl << "Input capture width (pixels): ";
-        //     cin >> captureWidth;
-        //     cout << endl << "Input capture height (pixels): ";
-        //     cin >> captureHeight;
-        //     cout << endl;
-
-        //     if (!captureWidth > 0) {
-        //         cout << endl << "Width value incorrect" << endl;
-        //         return -1;
-        //     }
-
-        //     if (!captureHeight > 0) {
-        //         cout << endl << "Height value incorrect" << endl;
-        //         return -1;
-        //     }
-        // }
     
 
     return 0;
